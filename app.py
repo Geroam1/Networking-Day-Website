43650589d746b3716ce9ddb01246682302b47bf8
Geroam 1 <502416@student.fontys.nl>
2025-11-02 15:04:52 +0100

    removed debug prints and added back buttons on the student and company page

--- app.py
+++ app.py
@@ -14,12 +14,10 @@ SCOPE = [
 
 creds_json = os.getenv("GOOGLE_CREDENTIALS") # will load the render secret if it exists
 if creds_json: 
-    print("GOOGLE_CREDENTIALS found in environment.")
     # running on Render or environment with secret set
     creds_info = json.loads(creds_json)
     CREDS = Credentials.from_service_account_info(creds_info, scopes=SCOPE)
 else:
-    print("GOOGLE_CREDENTIALS not found in environment.")
     # locally get creds with credentials.json file. TESTING ONLY
     CREDS = Credentials.from_service_account_file("credentials.json", scopes=SCOPE)
 
