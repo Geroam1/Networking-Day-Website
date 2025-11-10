af11369af307c8ebf3414770133b658c4b45999f
Geroam 1 <502416@student.fontys.nl>
2025-11-10 15:01:01 +0100

    updated the student and company profile dynamic pages to include smooth animations and a general info section, all that needs to be done now is to put the correct info into the data sheet and write up some good general information that the companies and students should know

--- static/js/AOS.js
+++ static/js/AOS.js
@@ -3,3 +3,10 @@ AOS.init({
 duration: 1000, // animation duration in ms
 once: true // animate only once when scrolling down
 });
+
+// Smooth scroll to input fields on window resize (for mobile devices)
+window.addEventListener('resize', () => {
+    if (document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA') {
+        document.activeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+});
